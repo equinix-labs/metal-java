7ed42d1d584e1b9d6dd7c139fdfe27558c7a9cc2
Vamshik Shetty <svamshik@vmware.com>
2022-08-01 21:06:19 +0530

    Add patch to introduce device state as enum

--- internal/workflow/DeviceOperator.java
+++ internal/workflow/DeviceOperator.java
@@ -5,14 +5,27 @@ import com.equinix.openapi.ApiException;
 import com.equinix.openapi.metal.v1.api.DevicesApi;
 import com.equinix.openapi.metal.v1.model.Device;
 import com.equinix.openapi.metal.v1.model.DeviceCreateInput;
+import com.equinix.openapi.metal.v1.model.Device.StateEnum;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class DeviceOperator {
    private DevicesApi deviceApiInstance;
+
+   /**
+    * After device get's deployed we get provisioning
+    * percentage as `null`. But during provisioning,
+    * percentage can be null even before provisioning
+    * even starts. Hence we should look at device state.
+    *
+    * Target : active or failed.
+    * Pending: queued, provisioning or reinstalling.
+    */
+   private final List<StateEnum> targetState = Arrays.asList(StateEnum.ACTIVE, StateEnum.FAILED);
 
    public DeviceOperator(ApiClient client) {
       this.deviceApiInstance = new DevicesApi(client);
@@ -35,8 +48,7 @@ public class DeviceOperator {
       UUID deviceId = null;
 
       List<String> empty = new ArrayList<>();
-      // After device get's deployed we get provisioning percentage as `null`
-      for(int i=0; (i < retries) && (device.getProvisioningPercentage() != null); i++) {
+      for(int i=0; (i < retries) && !targetState.contains(device.getState()); i++) {
          deviceId = device.getId();
          Thread.sleep(wait.toMillis());
          device = deviceApiInstance.findDeviceById(deviceId, empty, empty);
