405ca07caf188ccccf6d83e54f03d2a6da0c2c81
Vamshik Shetty <svamshik@vmware.com>
2022-08-01 15:52:23 +0530

    rename sdk workflows and add logging to tests

--- equinix-functional-test/src/test/java/com/equinix/test/functional/TestDeviceOperator.java
+++ equinix-functional-test/src/test/java/com/equinix/test/functional/TestDeviceOperator.java
@@ -1,6 +1,6 @@
 package com.equinix.functional.test;
 
-import com.equinix.workflow.DeviceManager;
+import com.equinix.workflow.DeviceOperator;
 import com.equinix.openapi.ApiClient;
 import com.equinix.openapi.ApiException;
 import com.equinix.openapi.Configuration;
@@ -18,17 +18,23 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.Assert;
 
-public class TestDeviceManager {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestDeviceOperator {
+
+   private static final Logger logger = LoggerFactory.getLogger(TestDeviceOperator.class);
    
    private final String propFilename = "src/test/resources/device-test.properties";
    private final String sysTokenKey = "FUNCTIONAL_TEST_TOKEN";
    private final String sysProjUuidKey = "FUNCTIONAL_TEST_PROJECT_UUID";
 
    private HashMap<String, String> props;
-   private DeviceManager deviceManager;
+   private DeviceOperator deviceOperator;
 
    @Before
    public void setup() {
+      logger.info("Executing TestDeviceOperator setup...");
       props = TestUtilis.ReadPropertiesFile(propFilename);
 
       // This properties needs to be added dynamically before test execution.
@@ -39,11 +45,12 @@ public class TestDeviceManager {
       ApiKeyAuth x_auth_token = (ApiKeyAuth) defaultClient.getAuthentication("x_auth_token");
       x_auth_token.setApiKey(token);
 
-      deviceManager = new DeviceManager(defaultClient);
+      deviceOperator = new DeviceOperator(defaultClient);
    }
 
    @Test
    public void testDeviceCreationAndPolling() throws ApiException, InterruptedException {
+      logger.info("Executing Test: testDeviceCreationAndPolling");
       UUID projId = UUID.fromString(System.getenv(sysProjUuidKey));
 
       DeviceCreateInput inp = new DeviceCreateInput()
@@ -55,21 +62,21 @@ public class TestDeviceManager {
 
       int retries = 2;
       Duration wait = Duration.ofSeconds(20);
-      System.out.println("Provisioning device ...");
+      logger.info("Provisioning device ...");
 
-      Device device = deviceManager.createDeviceAndPoll(inp, projId, retries, wait);
-      System.out.println(String.format("Device creation and ready completion status: %f", device.getProvisioningPercentage()));
+      Device device = deviceOperator.createDeviceAndPoll(inp, projId, retries, wait);
+      logger.info(String.format("Device creation and ready completion status: %f", device.getProvisioningPercentage()));
 
-      retries = 60;
+      retries = 120;
       wait = Duration.ofSeconds(10);
       if (device.getProvisioningPercentage() != null) {
-         device = deviceManager.deviceReadyAndPoll(device.getId(), retries, wait);
+         device = deviceOperator.deviceReadyAndPoll(device.getId(), retries, wait);
       }
-      System.out.println(String.format("Device ready completion status: %s", device.getProvisioningPercentage() == null));
+      logger.info(String.format("Device ready completion status: %s", device.getProvisioningPercentage() == null));
 
       // After device get's deployed we get provisioning percentage as `null`
       Assert.assertNull(device.getProvisioningPercentage());
 
-      deviceManager.deleteDevice(device.getId(), false);
+      deviceOperator.deleteDevice(device.getId(), false);
    }
 }
